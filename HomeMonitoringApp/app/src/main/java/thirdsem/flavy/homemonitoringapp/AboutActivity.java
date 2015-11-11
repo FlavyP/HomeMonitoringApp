2e858930c9a89a7159a0e1024b5935a9360a1bbe
Baveriddle <almightyartu@gmail.com>
2015-11-11 11:20:56 +0100

    11:03 rado

--- HomeMonitoringApp/app/src/main/java/thirdsem/flavy/homemonitoringapp/AboutActivity.java
+++ HomeMonitoringApp/app/src/main/java/thirdsem/flavy/homemonitoringapp/AboutActivity.java
@@ -1,12 +1,14 @@
 package thirdsem.flavy.homemonitoringapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextUtils;
 import android.text.TextWatcher;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -249,6 +251,30 @@ public class AboutActivity extends AppCompatActivity implements View.OnClickList
     @Override
     public void afterTextChanged (Editable editable) {
         checkText();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_flame, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Intent myIntent = new Intent( AboutActivity.this, MainActivity.class);
+            startActivity(myIntent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
 
