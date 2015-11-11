bd0d33806304fa4ac742f56c1fb232046e5179ff
Flavy <popaflavian7@gmail.com>
2015-11-11 20:08:28 +0100

    Fixed Main Activity buttons, added new scroll view.

--- HomeMonitoringApp/app/src/main/java/thirdsem/flavy/homemonitoringapp/MainActivity.java
+++ HomeMonitoringApp/app/src/main/java/thirdsem/flavy/homemonitoringapp/MainActivity.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
 public static final String TAG = "artuMSG";
 private Button butCon, butStatus,butLights,butFlame,butAbout;
@@ -19,101 +19,46 @@ private Button butCon, butStatus,butLights,butFlame,butAbout;
         setContentView(R.layout.activity_main);
         Log.i(TAG, "onCreate");
 
+        initializeComponents();
+    }
 
-
-
+    private void initializeComponents()
+    {
         butCon = (Button)findViewById(R.id.buttonCon);
+        butCon.setOnClickListener(this);
         butStatus = (Button)findViewById(R.id.buttonStatus);
+        butStatus.setOnClickListener(this);
         butLights = (Button)findViewById(R.id.buttonLights);
+        butLights.setOnClickListener(this);
         butFlame = (Button)findViewById(R.id.buttonFlame);
+        butFlame.setOnClickListener(this);
         butAbout = (Button)findViewById(R.id.buttonAbout);
+        butAbout.setOnClickListener(this);
+    }
 
-
-        butCon.setOnClickListener(new View.OnClickListener() {
-
-
-            @Override
-            public void onClick(View v) {
-                Intent myIntent = new Intent(MainActivity.this, BluetoothActivity.class);
-                startActivity(myIntent);
-
-
-                //Toast.makeText(MainActivity.this, "Hello from  Home Screen", Toast.LENGTH_LONG).show();
-
-
-            }
-
-
-        });
-
-        butStatus.setOnClickListener(new View.OnClickListener() {
-
-
-            @Override
-            public void onClick(View v) {
-                Intent myIntent = new Intent(MainActivity.this, StatusActivity.class);
-                startActivity(myIntent);
-
-
-                Toast.makeText(MainActivity.this, "Hello from  Home Screen", Toast.LENGTH_LONG).show();
-
-
-            }
-
-
-        });
-
-//        butLights.setOnClickListener(new View.OnClickListener() {
-//
-//
-//            @Override
-//            public void onClick(View v) {
-//                Intent myIntent = new Intent(MainActivity.this, LightsActivity.class);
-//                startActivity(myIntent);
-//
-//
-//                Toast.makeText(MainActivity.this, "Hello from  Home Screen", Toast.LENGTH_LONG).show();
-//
-//                finish();
-//            }
-//
-//
-//        });
-
-        butFlame.setOnClickListener(new View.OnClickListener() {
-
-
-            @Override
-            public void onClick(View v) {
-                Intent myIntent = new Intent(MainActivity.this, FlameActivity.class);
-                startActivity(myIntent);
-
-
-                Toast.makeText(MainActivity.this, "Hello from  Home Screen", Toast.LENGTH_LONG).show();
-
-
-            }
-
-
-        });
-
-        butAbout.setOnClickListener(new View.OnClickListener() {
-
-
-            @Override
-            public void onClick(View v) {
-                Intent myIntent = new Intent(MainActivity.this, AboutActivity.class);
-                startActivity(myIntent);
-
-
-                Toast.makeText(MainActivity.this, "Hello from  Home Screen", Toast.LENGTH_LONG).show();
-
-
-            }
-
-
-        });
-
-
+    @Override
+    public void onClick (View view) {
+        switch (view.getId()){
+            case R.id.buttonCon:
+                Intent bluetoothIntent = new Intent(MainActivity.this, BluetoothActivity.class);
+                startActivity(bluetoothIntent);
+                break;
+            case R.id.buttonAbout:
+                Intent aboutIntent = new Intent(MainActivity.this, AboutActivity.class);
+                startActivity(aboutIntent);
+                break;
+            case R.id.buttonFlame:
+                Intent flameIntent = new Intent(MainActivity.this, FlameActivity.class);
+                startActivity(flameIntent);
+                break;
+            case R.id.buttonLights:
+                //Intent lightIntent = new Intent(MainActivity.this, LightActivity.class);
+                //startActivity(lightIntent);
+                break;
+            case R.id.buttonStatus:
+                Intent statusIntent = new Intent(MainActivity.this, StatusActivity.class);
+                startActivity(statusIntent);
+                break;
+        }
     }
 }
